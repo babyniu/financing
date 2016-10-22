24bc76a83eed28be8015898592ab0342d94a67cc
yuejhan <yuejhan@163.com>
2016-10-22 15:51:38 +0800

    s''

---
+++ app/src/main/java/com/github/financing/utils/AppConst.java
@@ -0,0 +1,14 @@
+package com.github.financing.utils;
+
+/**
+ * Created by Crazz on 2/2/16.
+ */
+public class AppConst {
+    public static final String MCHNT_CD = "0002900F0338384";
+
+    private static final String MCHNT_BACK_NOTIFY_BASE_URL = "http://192.168.199.184:8080";
+    public static final String MCHNT_BACK_NOTIFY_URL_BACK = MCHNT_BACK_NOTIFY_BASE_URL+"/notify/api_back";
+    public static final String MCHNT_BACK_NOTIFY_URL_FRONT = MCHNT_BACK_NOTIFY_BASE_URL+"/notify/api_front";
+
+    public static final String MCHNT_RSA_PRIVATE_KEY = "MIICeAIBADANBgkqhkiG9w0BAQEFAASCAmIwggJeAgEAAoGBAO2u+9vkuQGYwe2Yw0XFXRdhCkvwY4328H2STJjeW2LGDJqYYQVbpw1CNwJ0hKKcpk1/APENbdv84RP7x3YEkQVNoB0uSj8qnpUsnuyBdxLdToohikvOrNRWgQx/ZvgFE+rWjka9wVaKqLUbUWxpq9GiUAPFa78kYOABD8dIMtg9AgMBAAECgYEAgpNzQiaxjLMDNyiJfrcioUlqgrWZu9BB5nqNIh5mTilHm1bDVlI3wAz0c6DXjQ5KPqDbP5KFHCoc7QGRXsC7egNBX9kNtL7ZCuYw78pE5sNM4+885fgoqaBCbnc+PxgyAqQ+ZIO5u6QKXQpEoe7PpvxCVBAGyn/1klaQVidUivECQQD87PnV05v8ibOv0N6cSpEZ8s/mdFVDSw0sFBdxMseFGY/WjDl1g9ZQCuwjrcT5S/mnYgb6MzRJn+s0rfCFlImLAkEA8JKURMVg6GqIleQq4e03uqEZ6AgErBlh2e+1/T9vgij6n/ueZysamHydZAupk3Wsfn1bkmdA4zqOCf7UZueOVwJAHDwIF8qrmyF0IahbcW8Ri6gDdWJ/MifqrIUBqO1WQJF98SFuOKQjBIRzn/gCCSJmGD1lMgENUTq88wCH3SGbyQJBAJzEuDAUe3EZM0aSOEufvQg2QV6OExVfOP+/ENYmB3FHaQLmAjRyx1MFKb9vRiMctLp80DaYaJVqq/Lhh+JDFOMCQQCXrBhjTx4KfLzfUhOVzm5D8w5sAn9Sg1jDeMwe8tyiyUBbbkw+k9qK0YLOfnwKuC3MNI5URjaKyLzilPDZZkrs";
+}
